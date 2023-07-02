93a0a39b25e1694135400bfb35c3b8f44567967a
Victoria <v9264500114@gmail.com>
2023-07-02 18:06:15 +0300

    fix commas, spaces

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,15 +1,15 @@
-
 const Card = require('../models/user');
+
 const { ERROR_CODE } = require('../utils/constsnts');
 
 const getAllCards = (req, res) => {
   Card.find({})
-      .then((card) => {
-        res.send(card)
-      })
-      .catch(() => {
-        res.status(ERROR_CODE.SERVER_ERROR).console.log('На сервере произошла ошибка');
-      });
+    .then((card) => {
+      res.send(card);
+    })
+    .catch(() => {
+      res.status(ERROR_CODE.SERVER_ERROR).console.log('На сервере произошла ошибка');
+    });
   console.log(req.body);
 };
 
@@ -17,15 +17,15 @@ const createCard = (req, res) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner: req.user._id })
-      .then((card) => {
-        res.send(card)
-      })
-      .catch((err) => {
-        if (err.name === 'ValidationError') {
-          res.status(ERROR_CODE.BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании карточки.' });
-        } else {
-          res.status(ERROR_CODE.SERVER_ERROR).send({ message: 'На сервере произошла ошибка' });
-        }
+    .then((card) => {
+      res.send(card);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_CODE.BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании карточки.' });
+      } else {
+        res.status(ERROR_CODE.SERVER_ERROR).send({ message: 'На сервере произошла ошибка' });
+      }
   });
 
   console.log(req.user._id);
