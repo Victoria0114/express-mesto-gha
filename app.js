79a783bb5e485b58d6d56ecfaede77a6a75701a8
Victoria <v9264500114@gmail.com>
2023-07-02 16:43:45 +0300

    fix semicolon test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
 
 app.use((req, res, next) => {
-  req.user = {_id: '649d6d1a759ddbd827b0d76c'};
+  req.user = { _id: '649d6d1a759ddbd827b0d76c' };
   next();
 });
 
@@ -27,43 +27,4 @@ app.use('/', (req, res) => {
 app.listen(PORT, () => {
   console.log('Сервер запущен');
   console.log(`App listening on port ${PORT}`);
-})
-
-
-
-
-
-
-
-
-
-// // Устанавливаем точку входа
-// // const express = require('express');
-
-// // const app = express();
-// // создали express-приложение.
-
-// const express = require('express');
-
-// // Слушаем 3000 порт
-// const { PORT = 3000 } = process.env;
-
-// const mongoose = require('mongoose');
-
-// const app = express();
-
-
-// // подключаемся к серверу mongo
-// mongoose.connect('mongodb://localhost:27017/mestodb', {
-//   useNewUrlParser: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false
-// });
-
-// // подключаем мидлвары, роуты и всё остальное...
-
-// app.listen(PORT, () => {
-//     // Если всё работает, консоль покажет, какой порт приложение слушает
-//     console.log(`App listening on port ${PORT}`)
-// })
-
+});
