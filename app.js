288d57ede5a8e54429e637e19143817885d328e2
Victoria <v9264500114@gmail.com>
2023-07-03 13:47:05 +0300

    fix order

--- app.js
+++ app.js
@@ -14,13 +14,14 @@ console.log('Connected to db');
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
 
 app.use((req, res, next) => {
   req.user = { _id: '649d6d1a759ddbd827b0d76c' };
   next();
 });
+
+app.use('/users', usersRouter);
+app.use('/cards', cardsRouter);
 
 app.use('/', (req, res) => {
   res.status(ERROR_CODE.NOT_FOUND).send({ message: 'Страница не найдена' });
