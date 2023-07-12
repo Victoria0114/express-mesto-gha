d4f3648ec944f74ee3328c68cafd7e8a91670f70
Victoria <v9264500114@gmail.com>
2023-07-12 14:24:52 +0300

    update routes

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { cardIdValidate, cardValidate } = require('../middlewares/validation');
 
 const {
   getAllCards,
@@ -9,9 +10,9 @@ const {
 } = require('../controllers/cards');
 
 router.get('/', getAllCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', putLike);
-router.delete('/:cardId/likes', deleteLike);
+router.post('/', cardValidate, createCard);
+router.delete('/:cardId', cardIdValidate, deleteCard);
+router.put('/:cardId/likes', cardIdValidate, putLike);
+router.delete('/:cardId/likes', cardIdValidate, deleteLike);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,20 @@
 const router = require('express').Router();
 
 const {
-  createUser,
+  //createUser,
   getAllUsers,
   getUser,
   updateUser,
   updateUserAvatar,
 } = require('../controllers/users');
 
-router.post('/', createUser);
-router.get('/', getAllUsers);
-router.get('/:id', getUser);
-router.patch('/me', updateUser);
-router.patch('/me/avatar', updateUserAvatar);
+const { userValidate, userIdValidate, avatarValidate } = require('../middlewares/validation');
 
-router.get('/me', updateUser);
+//router.post('/', createUser);
+router.get('/', getAllUsers);
+router.get('/me', getUser);
+router.get('/:id', userIdValidate, getUser);
+router.patch('/me', userValidate, updateUser);
+router.patch('/me/avatar', avatarValidate, updateUserAvatar);
 
 module.exports = router;
