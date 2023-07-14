9388d97fb48acb8af33f55cf737d8be8ee353630
Victoria <v9264500114@gmail.com>
2023-07-14 17:20:56 +0300

    test

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,5 +1,6 @@
 const error = (err, req, res, next) => {
   const { status = 500, message } = err;
+  console.log(err);
 
   res.status(status).send({
     message: status === 500 ? 'Ошибка сервера' : message,
